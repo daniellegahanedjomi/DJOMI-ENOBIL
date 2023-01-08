aa5d734dfe86223f0a41a3664d74397b7faa5bb7
danielledjomi <djomigahane@et.esiea.fr>
2023-01-08 18:26:57 +0100

    partie1

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=danielle
+export POSTGRES_PASSWORD=danielle
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=database
+export AWS_BUCKET=udagram-esiea-2.0
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
